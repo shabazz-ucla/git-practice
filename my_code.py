5eca6cce0bd1eaf3432f97a465dd703a381c63cb
Adam Millard-Ball <adammb@ucsc.edu>
2022-04-23 14:58:43 -0700

    Added file

---
+++ my_code.py
@@ -0,0 +1,9 @@
+"""
+Here's a docstring telling you what this file is about
+"""
+
+def print_text(text):
+    for i in range(100):
+        print(text)
+
+print_text('Data science is really fun.')  
