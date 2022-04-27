4ef38bf6fe1a15b04b6ba874c3cb5ef657762f94
TiffanyGreen <37752009+TiffanyGreen@users.noreply.github.com>
2022-04-27 10:38:10 -0700

    Update code.py

--- my_code.py
+++ my_code.py
@@ -4,6 +4,6 @@ Here's a docstring telling you what this file is about
 
 def print_text(text):
     for i in range(100):
-        print(text+ '..especially when the code works!')
+        print(text+ '..especially when the code works! But that does not always happen, sadly.')
 
 print_text('Data science is really fun.')  
