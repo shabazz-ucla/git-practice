b0ee1ebdfa6d801c0a66e4d41da5435030a83f7f
Adam Millard-Ball <adammb@ucla.edu>
2022-04-27 10:38:00 -0700

    changed mycode

--- my_code.py
+++ my_code.py
@@ -4,6 +4,6 @@ Here's a docstring telling you what this file is about
 
 def print_text(text):
     for i in range(100):
-        print(text+ '..especially when the code works!')
+        print(text+ '..especially when the code works. Possibly!')
 
 print_text('Data science is really fun.')  
